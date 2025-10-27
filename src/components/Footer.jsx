e32a5b85161f9f85441420f3a6f747c375500f5d
Sk Abadat Haque <sksaddam7224@gmail.com>
2025-10-28 00:20:25 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,27 @@
-export default function Footer(){
-return  <footer className="site-footer">
-        <p>© {new Date().getFullYear()} Luminex Sparq Company — Bagh Chingari.</p>
-      </footer>
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <p>
+          © {new Date().getFullYear()}{" "}
+          <span className="brand">Luminex Sparq Company — Bagh Chingari</span>
+        </p>
+        <p>
+          Crafted with ❤️ by{" "}
+          <a
+            href="https://www.linkedin.com/in/abadat-dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="author"
+          >
+            Sk Abadat Haque
+          </a>
+          .
+        </p>
+      </div>
+      <div className="footer-line"></div>
+
+ 
+    </footer>
+  );
 }
